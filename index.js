5795b5ff4899da18c7300eefa11ccdd64cb218d5
rekha shejul <rekhashejul804@gmail.com>
2025-02-25 13:42:59 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req , res) => {
+    res.send("hii i am termineter")
+} )
+
+app.get('/login', (req , res) => {
+    res.send("<h1>please login at bhushanbackend</h1>")
+} )
+
+app.get('/game', (req , res) => {
+    res.send("You won play with me")
+} )
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
